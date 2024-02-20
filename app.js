47eee166b00d1db8bc1ce922fa48116a5b5af1e7
mmk <mmkkarbalaei@gmail.com>
2024-02-20 22:56:57 +0330

    createTask

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let button = document.querySelector('button');
 let input = document.querySelector('input');
 let TaskList = document.querySelector('ul');
+
+function createTask(text){
+    let li = document.createElement('li');
+    li.textContent = text;
+    return li;
+}
