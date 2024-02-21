927b8ca8f1486e08e129916ea63179d2758c73d6
mmk <mmkkarbalaei@gmail.com>
2024-02-21 09:09:21 +0330

    fix bug

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ let button = document.querySelector('button');
 let input = document.querySelector('input');
 let TaskList = document.querySelector('ul');
 
-function createTask(text){
+function createTask(text) {
     let li = document.createElement('li');
     li.textContent = text;
     return li;
 }
 
-button.addEventListener('click',()=>{
+button.addEventListener('click', () => {
     let text = input.value;
-    let task = createTask(text);
-    task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>';
-    TaskList.appendChild(task);
-    input.value='';
+    if (!(text === '')) {
+        let task = createTask(text);
+        task.innerHTML += '<span class="closeBtn"><i class="fa-solid fa-trash-can"></i></span>';
+        TaskList.appendChild(task);
+        input.value = '';
+    }
 })
 
-TaskList.addEventListener('click',(event)=>{
-    if(event.target.nodeName === 'I')
+TaskList.addEventListener('click', (event) => {
+    if (event.target.nodeName === 'I')
         event.target.parentElement.parentElement.style.display = 'none';
-    if(event.target.nodeName === 'LI'){
+    if (event.target.nodeName === 'LI') {
         event.target.classList.toggle('done');
     }
 })
